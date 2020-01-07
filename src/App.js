4492fb45ee277d06a91afae3aa0fe2bdadcf10a1
MedJawad <mohamedjawad.aatafay@gmail.com>
2020-01-07 23:28:10 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        {/* <Navbar />
-        <Main /> */}
+         <Navbar />
+       {/* <Main /> */}
         <Footer />
     </div>
   );
