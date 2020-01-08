ebed3bc8244ac357f6aa9cc9f9705f51eeb1a3e3
MedJawad <mohamedjawad.aatafay@gmail.com>
2020-01-08 12:18:20 +0100

    added Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,23 @@
 import React from 'react';
-
+import {
+    Card,
+    Button    } from 'react-bootstrap';
 
 class Footer extends React.Component {
 
 
     render(){
         return (
-            <div>
-                Hello World !
-            </div>
+            <Card className="text-center fixed-bottom">
+
+                <Card.Body>
+                    <Card.Title>🤷‍♂️ You've reached the end of current articles 🤷‍♂️</Card.Title>
+                    <Card.Text>
+                        Follow More Rooms To get To see more amazing stuff on ForUs.
+                    </Card.Text>
+                    <Button variant="primary">Go to Top</Button>
+                </Card.Body>
+            </Card>
         )
     }
 }
